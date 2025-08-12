3f82c31367683e4edffa9a323454d4b5fc07c8a0
Menna-Hassan246 <146475372+Menna-Hassan246@users.noreply.github.com>
2025-08-12 20:41:04 +0300

    Add files via upload

---
+++ OOP_Tasks/Task_01/SavingAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class SavingAccount:BankAccount
+    {
+        
+            public decimal InterestRate { get; set; }
+
+            public SavingAccount() : base()
+            {
+                InterestRate = 0;
+            }
+
+            public SavingAccount(int acc,string fullname, string national_ID, string phonenumber, string address, int balance, decimal interestRate)
+                : base( acc,fullname, national_ID, phonenumber, address, balance)
+            {
+                InterestRate = interestRate;
+            }
+
+            public override decimal CalculateInterest()
+            {
+                return Balance * InterestRate / 100;
+            }
+
+            public override void ShowAccountDetails()
+        {
+            base.ShowAccountDetails();
+            Console.WriteLine($"Interest Rate: {InterestRate}%");
+            
+        }
+
+    }
+}
