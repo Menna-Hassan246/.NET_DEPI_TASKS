dc9cfd45e660353c37dd98e1c1e7a8d48eed34f2
Menna-Hassan246 <146475372+Menna-Hassan246@users.noreply.github.com>
2025-09-15 19:21:22 +0300

    Add files via upload

---
+++ API__Tasks/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Options;
+
+namespace Task1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        { 
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure SSN as the primary key
+            modelBuilder.Entity<Employee>()
+                .HasKey(e => e.SSN);
+            // Configure FName as required with max length of 50
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.FName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Configure LName as required with max length of 50
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.LName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Configure Salary with precision (from previous question)
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Salary)
+                .HasPrecision(18, 2);
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
