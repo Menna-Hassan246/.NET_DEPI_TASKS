affa3cd427ecbf1dccfca1f3bec1a77ed28d988c
Menna-Hassan246 <146475372+Menna-Hassan246@users.noreply.github.com>
2025-09-21 15:21:46 +0300

    Add files via upload

---
+++ API_Tasks/Task_02/Controllers/EmployeesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task1.Data;
+using Task1.Interfaces;
+
+namespace Task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmplyeeService _Service;
+        public EmployeesController(IEmplyeeService Service)
+        {
+            _Service = Service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetallEmplyees()
+        {
+            var emps=await _Service.GetAll();
+            return Ok(emps);
+        }
+        [HttpGet("SSN")]
+        public async Task<IActionResult> GetBySSN(string ssn )
+        {
+            var emps = await _Service.GetBySSN(ssn);
+            return Ok(emps);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateNewEmp(Employee employee)
+        {
+            Employee emp = new Employee()
+            { SSN = employee.SSN,FName=employee.FName,LName=employee.LName,Salary=employee.Salary };
+            await _Service.CreateNewone(employee);
+            return Ok(emp);
+        }
+        [HttpPut]
+        public async Task<IActionResult>UpdateEmpSalary(string ssn,Employee e)
+        {
+            var emp= await _Service.UpdateEmp(ssn,e);
+            if (emp == null)
+            {
+                return NotFound($"Employee SSN {ssn} not exist ");
+            }
+            emp.Salary = e.Salary;
+            return Ok(emp);
+        }
+        [HttpDelete("SSN")]
+        public async Task<IActionResult>DeleteEmp(string ssn)
+        {
+            var emp=await _Service.DeleteEmp(ssn);
+            if (emp == null)    
+            {
+                return NotFound($"Employee SSN {ssn} not exist ");
+            }
+             return Ok(emp) ;
+        }
+    }
+}
---
+++ API_Tasks/Task_02/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Task1.Data
+{
+    public class Employee
+    {
+        public string  SSN { get; set; }
+       
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public decimal Salary { get; set; }
+        
+    }
+}
